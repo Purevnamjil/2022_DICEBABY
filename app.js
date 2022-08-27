3e234f1bab53fbf934706f4faf82231e52b6605a
Puujee <bpurevnamjil@gmail.com>
2022-08-27 21:29:31 +0800

    Шоо хаяж болдог болов

--- app.js
+++ app.js
@@ -8,14 +8,31 @@ var scores = [0,0];
 var roundScore = 0;
 
 //Шооны аль талаарай буусныг хадгалах 
-var diceSide = Math.floor(Math.random()*6) + 1 ;
+
 
 //Программ эхлэхэд бэлтгэх
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
 
-
-
+//Шоог арилгах
 document.querySelector(".dice").style.display = "none";
+
+//dom listener
+var domDice = document.querySelector(".dice");
+
+
+// Товчны эвэнт листенер
+document.querySelector(".btn-roll").addEventListener( "click", function ()
+{
+var diceSide = Math.floor(Math.random()*6) + 1 ;
+
+domDice.style.display = "block";
+domDice.src = "dice-" + diceSide + ".png";
+
+
+}
+);
+
+
